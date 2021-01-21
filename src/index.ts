50c5d0b27bb279d00140ac9e9fdc6820e3ab91e5
HongJunHyeok <edb1631@naver.com>
2021-01-21 21:31:31 +0900

    [ADD]expressing types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,48 @@
-const name = 'hong',
-  age = 10,
-  gender = 'male';
-const sayHi = (name: string, age: number, gender?: string): void => {
-  console.log(`Hello ${name} , ${age}years old, ${gender}`);
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+//or
+
+type Human = {
+  name: string;
+  age: number;
+  gender: string;
 };
 
-sayHi(name, age, gender);
+//or expressing js
+
+class Human_2 {
+  name: string;
+  age: number;
+  gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const sechan: Person = {
+  name: 'sechan',
+  age: 18,
+  gender: 'male',
+};
+const minsu: Human = {
+  name: 'minsu',
+  age: 18,
+  gender: 'male',
+};
+const hong = new Human_2('hong', 18, 'male'); //use Class
+
+const sayHi = (object: Person): string => {
+  return `Hello ${object.name} , ${object.age}years old, ${object.gender}`;
+};
+
+console.log(sayHi(sechan));
+console.log(sayHi(minsu));
+console.log(sayHi(hong));
 
 export {};
